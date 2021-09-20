d8a65081e6cbc786001403786a48e3e56313d5c0
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-09-20 10:09:32 -0500

    More configurations of Flask

--- pruv.py
+++ pruv.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/primeraruta')
+@app.route('/ruta')
 def alguien_entro_a_la_ruta():
-    return 'Mi primer backend!!!!'
+    return 'Hola daniel'
