55d1513acf1c3cf3a9b308af533f1662d381b9a0
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-09-27 09:23:49 -0500

    DB Conect

--- models.py
+++ models.py
@@ -3,8 +3,13 @@ from pruv import db
 # Tabla aviones
 class Aviones(db.Model): 
     __tablename__ = "Aviones"
-    id = db.Column(db.Integer, primary_Key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String)
     añoFabricacion = db.Column(db.String)
     empresa = db.Column(db.String)
+
+    def __init__(self, nombre, añoFabricacion, empresa):
+        self.nombre = nombre
+        self.añoFabricacion = añoFabricacion
+        self.empresa = empresa
     
--- pruv.py
+++ pruv.py
@@ -1,9 +1,22 @@
+
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/birdbd'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'some-secret-key'
+
+
 db=SQLAlchemy(app)
+
+from models import Aviones
+
+db.create_all()
+db.session.commit()
+
+
 
 @app.route('/ruta')
 def alguien_entro_a_la_ruta():
@@ -22,6 +35,16 @@ def ruta_raiz():
 @app.route('/bird', methods=["GET","POST"])
 def crud_Bird():
     if request.method=="GET":
+
+        nombre = "icaro"
+        fecha= "12-23-21"
+        empresa="avianca"
+
+        avion = Aviones(nombre, fecha, empresa)
+
+        db.session.add(avion)
+        db.session.commit()
+
         print("Se solicito un avion")
         return "Nos soliccitan un avion"
     elif request.method=="POST":
